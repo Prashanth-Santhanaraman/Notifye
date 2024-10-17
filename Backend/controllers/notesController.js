4205851cb6a975524a11df7a6d562e90fa57185f
Prashanth <73587553+Prashanth-Santhanaraman@users.noreply.github.com>
2024-10-17 17:46:30 +0530

    Added funtions for updateNote and deleteNote
    Added functionality for updateNote and deleteNote in notesController.js

--- Backend/controllers/notesController.js
+++ Backend/controllers/notesController.js
@@ -1,28 +1,28 @@
 const notesModel = require("../models/notesModel");
-
+const mongoose = require("mongoose");
 // to get all notes
-const getNotes = async(req, res) => {
-    try {
-        const allNotes = await notesModel.find({})
-        res.status(200).json(allNotes)
-    } catch (error) {
-        res.status(400).json({message:error.message})
-    }
+const getNotes = async (req, res) => {
+  try {
+    const allNotes = await notesModel.find({});
+    res.status(200).json(allNotes);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 //to get particular note by using ID
-const getNote = async(req, res) => {
-    const {id} = req.params
-    try {
-        const oneNote = await notesModel.findById(id)
-        if(!oneNote){
-            return res.status(404).json({message:"Note not found"})
-        }
-        res.status(200).json(oneNote)
-    } catch (error) {
-        res.status(400).json({message:error.message})
+const getNote = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const oneNote = await notesModel.findById(id);
+    if (!oneNote) {
+      return res.status(404).json({ message: "Note not found" });
     }
-//   res.json({ message: "Got the one get request" });
+    res.status(200).json(oneNote);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+  //   res.json({ message: "Got the one get request" });
 };
 
 // to post a note
@@ -39,13 +39,44 @@ const newNote = async (req, res) => {
 };
 
 // to update a note
-const updateNote = (req, res) => {
-  res.json({ message: "Got the Update request" });
+const updateNote = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: "No such note" });
+  }
+
+  const note = await notesModel.findByIdAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!note) {
+    res.status(400).json({ message: "No such note to update" });
+  }
+
+  res.status(200).json(note);
+  // res.json({ message: "Got the Update request" });
 };
 
 //to delete a note
-const deleteNote = (req, res) => {
-  res.json({ message: "Got the Delete request" });
+const deleteNote = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: "No such note" });
+  }
+
+  const note = await notesModel.findByIdAndDelete({ _id: id });
+
+  if (!note) {
+    res.status(400).json({ message: "No such note to delete" });
+  }
+
+  res.status(200).json(note);
+  // res.json({ message: "Got the Delete request" });
 };
 
 module.exports = { getNotes, getNote, newNote, updateNote, deleteNote };
