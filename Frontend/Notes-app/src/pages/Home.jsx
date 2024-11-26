7f8c64dfa1c654c6cb99d042e495725bb985b419
Prashanth <73587553+Prashanth-Santhanaraman@users.noreply.github.com>
2024-11-26 21:35:28 +0530

    Changed the UI for card and added functionality of edit button
    Changed the UI for the card. edit button functionality is enabled.

--- Frontend/Notes-app/src/pages/Home.jsx
+++ Frontend/Notes-app/src/pages/Home.jsx
@@ -6,6 +6,13 @@ export default function Home() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [deleteNoteId, setDeleteNoteId] = useState("");
+  const [updateTitle, setUpdateTitle] = useState("");
+  const [updateDescription, setUpdateDescription] = useState("");
+  const [editButton, setEditButton] = useState(false);
+
+  const [editTitle, setEditTitle] = useState("");
+  const [editDescription, setEditDescription] = useState("");
+  const [editId, setEditId] = useState("");
   const token = localStorage.getItem("token");
 
   useEffect(() => {
@@ -52,7 +59,7 @@ export default function Home() {
     // console.log(id)
     try {
       const response = axios
-        .delete(`http://localhost:5000/api/notes/${id}`,{
+        .delete(`http://localhost:5000/api/notes/${id}`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
@@ -60,19 +67,43 @@ export default function Home() {
         .then((res) => console.log(res))
         .catch((err) => console.error(err));
 
-      if(response.status === 200){
-        setUserNotes(userNotes.filter(note => note._id !== id))
+      if (response.status === 200) {
+        setUserNotes(userNotes.filter((note) => note._id !== id));
       }
     } catch (err) {
       console.error("Error deleting note:", err);
     }
   };
+
+  const handleUpdateNote = (e) => {
+    e.preventDefault();
+    try {
+      axios
+        .patch(
+          `http://localhost:5000/api/notes/${editId}`,
+          {
+            title: editTitle,
+            description: editDescription,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((response) => console.log(response.data))
+        .catch((error) => console.error(error));
+    } catch (err) {}
+  };
   return (
     <>
       <div class="fixed bottom-7 right-7 flex items-center justify-center">
         <button
-          className="btn btn-square btn-lg btn-neutral"
-          onClick={() => setAddNotes(!addNotes)}
+          className="btn btn-square btn-md btn-square"
+          onClick={() => {
+            setEditButton(false);
+            setAddNotes(!addNotes)
+          }}
         >
           +
         </button>
@@ -80,15 +111,33 @@ export default function Home() {
       {!addNotes ? (
         <div className="flex flex-wrap gap-4 p-5">
           {userNotes.map((note) => (
-            <div key={note._id} className="card bg-purple-200 w-96 shadow-md">
+            <div
+              key={note._id}
+              className="card border border-slate-950 w-80 rounded-md"
+            >
               <div className="card-body">
-                <h1 className="card-title">
-                  {note.title}{" "}
-                  <details className="dropdown">
-                    <summary className="btn">:</summary>
-                    <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                <div className="flex justify-between items-center">
+                  <h1 className="card-title">{note.title}</h1>
+                  <div className="dropdown dropdown-end">
+                    <button tabIndex={0} className="btn btn-square btn-sm">
+                      ⋮
+                    </button>
+                    <ul
+                      tabIndex={0}
+                      className="dropdown-content menu bg-base-100 rounded-box w-40 p-2 shadow"
+                    >
                       <li>
-                        <a>Edit</a>
+                        <button
+                          onClick={() => {
+                            setEditButton(true);
+                            setAddNotes(!addNotes);
+                            setEditId(note._id);
+                            setEditTitle(note.title);
+                            setEditDescription(note.description);
+                          }}
+                        >
+                          Edit
+                        </button>
                       </li>
                       <li>
                         <button
@@ -97,12 +146,12 @@ export default function Home() {
                             handleDeleteNote(note._id);
                           }}
                         >
-                          Delete note
+                          Delete Note
                         </button>
                       </li>
                     </ul>
-                  </details>
-                </h1>
+                  </div>
+                </div>
                 <p>{note.description}</p>
               </div>
             </div>
@@ -111,64 +160,133 @@ export default function Home() {
       ) : (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
           <div className="card w-96 bg-white shadow-lg p-6">
-            <div className="flex items-center justify-end">
-              <button
-                className="btn btn-circle btn-sm btn-neutral flex"
-                onClick={() => setAddNotes(!addNotes)}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-            <form>
-              <div className="form-control mb-4">
-                <label className="label">
-                  <span className="label-text">Title</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Enter the title"
-                  className="input input-bordered w-full"
-                  onChange={(e) => {
-                    setTitle(e.target.value);
-                  }}
-                  required
-                />
-              </div>
-              <div className="form-control mb-4">
-                <label className="label">
-                  <span className="label-text">Description</span>
-                </label>
-                <textarea
-                  className="textarea textarea-bordered"
-                  placeholder="Enter the description"
-                  maxLength={350}
-                  onChange={(e) => {
-                    setDescription(e.target.value);
-                  }}
-                ></textarea>
-              </div>
+            {!editButton ? (
+              <>
+                <div className="flex items-center justify-between">
+                  <h2 className="font-bold text-2xl pt-2">New Note</h2>
+                  <button
+                    className="btn btn-circle btn-sm btn-square flex"
+                    onClick={() => setAddNotes(!addNotes)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-4 w-4"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                <form>
+                  <div className="form-control mb-4">
+                    <label className="label">
+                      <span className="label-text">Title</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Enter the title"
+                      className="input input-bordered w-full"
+                      onChange={(e) => {
+                        setTitle(e.target.value);
+                      }}
+                      required
+                    />
+                  </div>
+                  <div className="form-control mb-4">
+                    <label className="label">
+                      <span className="label-text">Description</span>
+                    </label>
+                    <textarea
+                      className="textarea textarea-bordered"
+                      placeholder="Enter the description"
+                      maxLength={350}
+                      onChange={(e) => {
+                        setDescription(e.target.value);
+                      }}
+                    ></textarea>
+                  </div>
 
-              <button
-                type="submit"
-                className="btn btn-neutral w-full"
-                onClick={handleSubmit}
-              >
-                Add
-              </button>
-            </form>
+                  <button
+                    type="submit"
+                    className="btn btn-neutral w-full"
+                    onClick={handleSubmit}
+                  >
+                    Add
+                  </button>
+                </form>
+              </>
+            ) : (
+              <>
+                <div className="flex items-center justify-between">
+                  <h2 className="font-bold text-2xl pt-2">Edit Note</h2>
+                  <button
+                    className="btn btn-circle btn-sm btn-square flex"
+                    onClick={() => setAddNotes(!addNotes)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-4 w-4"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                <form>
+                  <div className="form-control mb-4">
+                    <label className="label">
+                      <span className="label-text">Title</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Enter the title"
+                      className="input input-bordered w-full"
+                      value={editTitle}
+                      onChange={(e) => {
+                        setEditTitle(e.target.value);
+                      }}
+                      required
+                    />
+                  </div>
+                  <div className="form-control mb-4">
+                    <label className="label">
+                      <span className="label-text">Description</span>
+                    </label>
+                    <textarea
+                      className="textarea textarea-bordered"
+                      placeholder="Enter the description"
+                      maxLength={350}
+                      value={editDescription}
+                      onChange={(e) => {
+                        setEditDescription(e.target.value);
+                      }}
+                    ></textarea>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="btn btn-neutral w-full"
+                    onClick={handleUpdateNote}
+                  >
+                    Update
+                  </button>
+                </form>
+              </>
+            )}
           </div>
         </div>
       )}
