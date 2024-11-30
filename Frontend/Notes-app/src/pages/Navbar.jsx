e1cb938823c65144a3fd9c916bf2069ced13139a
Prashanth <73587553+Prashanth-Santhanaraman@users.noreply.github.com>
2024-11-30 21:18:51 +0530

    Installed Redux package
    Installed redux package and started storing the token and login status.

--- Frontend/Notes-app/src/pages/Navbar.jsx
+++ Frontend/Notes-app/src/pages/Navbar.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Navbar({}) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
-
+  const loggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const localLoggedIn = localStorage.getItem("isLoggedIn") === 'true'
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -12,22 +14,27 @@ export default function Navbar({}) {
     }
   }, []);
 
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   const handleLogout = (e) => {
     e.preventDefault();
     localStorage.removeItem("token");
+    localStorage.setItem("isLoggedIn","false")
     setIsLoggedIn(false);
     navigate("/");
   };
 
   return (
     <>
-      <div className="navbar bg-neutral text-neutral-content">
+      <div className="navbar bg-neutral px-[20px] text-neutral-content">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">Notes</a>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
-            {!isLoggedIn ? (
+            {!localLoggedIn ? (
               <>
                 <li>
                   <a href="/" className="btn btn-ghost">
@@ -42,9 +49,8 @@ export default function Navbar({}) {
               </>
             ) : (
               <>
-                
                 <li>
-                  <button onClick={handleLogout} className="btn btn-ghost">
+                  <button onClick={handleLogout} className="btn btn-nuetral">
                     Logout
                   </button>
                 </li>
---
+++ Frontend/Notes-app/src/store/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  token: null,
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.token = action.payload;
+      state.isLoggedIn = true;
+    },
+    logout: (state) => {
+      state.token = null;
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
