3347e28b18ee19c7d428bd70c8d601e9dc80879a
Prashanth <73587553+Prashanth-Santhanaraman@users.noreply.github.com>
2024-12-04 13:23:12 +0530

    without login can not access the routes
    without logging in the user can not access the routes. So introduced protected routes.

--- Frontend/Notes-app/src/App.jsx
+++ Frontend/Notes-app/src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import ProtectedRoute from "./routes/protectedRoute";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Navbar from "./pages/Navbar";
 import Home from "./pages/Home";
+import Settings from "./pages/Settings";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,7 +19,23 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/home/settings"
+          element={
+            <ProtectedRoute>
+              <Settings />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
 
