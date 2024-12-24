5efe6e551970dbc30ad78b8136d3f003b4c9344a
Prashanth <73587553+Prashanth-Santhanaraman@users.noreply.github.com>
2024-12-24 17:57:11 +0530

    Changes in settings
    Added functionality for password change.

--- Frontend/Notes-app/src/pages/Settings.jsx
+++ Frontend/Notes-app/src/pages/Settings.jsx
@@ -1,11 +1,18 @@
 import { useNavigate } from "react-router-dom";
-import ChangePassword from "./ChangePassword";
-import DeleteAccount from "./DeleteAccount";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
 
 export default function Settings() {
   const [passwordChange, setPasswordChange] = useState(false);
   const [deleteAccount, setDeleteAccount] = useState(false);
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const token =
+    useSelector((state) => state.auth.token) || localStorage.getItem("token");
   const navigate = useNavigate();
   const handlePasswordChange = () => {
     setPasswordChange(true);
@@ -15,8 +22,37 @@ export default function Settings() {
     setDeleteAccount(true);
     setPasswordChange(false);
   };
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "http://localhost:5000/api/account/changePassword",
+        {
+          currentPassword: currentPassword,
+          newPassword: newPassword,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        toast.success("Password Changed Successfully")
+        setCurrentPassword("");
+        setNewPassword("");
+      })
+      .catch((err) => {
+        console.error(err)
+        toast.error(err.response.data.message)
+      });
+  };
   return (
     <>
+      <div>
+        <Toaster position="top-center" />
+      </div>
       <div class="flex h-[80vh]">
         <div class="border-r text-neutral-content w-64 flex-shrink-0 hidden md:block">
           <div class="p-4 text-xl font-bold text-black border-b-2 flex items-center gap-2">
@@ -64,10 +100,91 @@ export default function Settings() {
         {!deleteAccount ? (
           <div class="flex-grow p-4 bg-base-100">
             <h1 class="text-2xl font-bold mb-4">Change Password</h1>
-            <p>
-              Welcome to the sidebar layout example. Use this space for your
-              main content.
-            </p>
+            <div className="flex flex-col w-[500px] p-[20px]">
+              <form>
+                <div className="form-control mb-2">
+                  <label className="label">
+                    <span className="label-text">Current Password</span>
+                  </label>
+                  <input
+                    type={!showPassword ? "password" : "text"}
+                    placeholder="Enter your current password"
+                    className={`input input-bordered w-full ${
+                      passwordError ? "input-error" : ""
+                    }`}
+                    value={currentPassword}
+                    onChange={(e) => setCurrentPassword(e.target.value)}
+                    required
+                  />
+                  {passwordError ? (
+                    <>
+                      <div className="label">
+                        <span className="label-text-alt text-red-600">
+                          Enter password
+                        </span>
+                      </div>
+                    </>
+                  ) : (
+                    " "
+                  )}
+                  <label className="label cursor-pointer w-[150px]">
+                    <input
+                      type="checkbox"
+                      className="checkbox checkbox-sm"
+                      onChange={() => setShowPassword(!showPassword)}
+                    />
+                    <span className="label ml-0 text-sm">Show Password</span>
+                  </label>
+                </div>
+
+                <div className="form-control mb-6">
+                  <label className="label">
+                    <span className="label-text">New Password</span>
+                  </label>
+                  <input
+                    type={!showPassword ? "password" : "text"}
+                    placeholder="Enter your new password"
+                    className={`input input-bordered w-full ${
+                      passwordError ? "input-error" : ""
+                    }`}
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                    required
+                  />
+                  {passwordError ? (
+                    <>
+                      <div className="label">
+                        <span className="label-text-alt text-red-600">
+                          Enter password
+                        </span>
+                      </div>
+                    </>
+                  ) : (
+                    " "
+                  )}
+                  <label className="label cursor-pointer w-[150px]">
+                    <input
+                      type="checkbox"
+                      className="checkbox checkbox-sm"
+                      onChange={() => setShowPassword(!showPassword)}
+                    />
+                    <span className="label ml-0 text-sm">Show Password</span>
+                  </label>
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-neutral w-full"
+                  onClick={handleUpdate}
+                >
+                  Update
+                </button>
+              </form>
+              {/* <label>Current Password:</label>
+              <input type="text" />
+
+              <label>New Password:</label>
+              <input type="text" /> */}
+            </div>
           </div>
         ) : (
           <div class="flex-grow p-4 bg-base-100">
@@ -85,7 +202,8 @@ export default function Settings() {
               <label className="form-control flex flex-row gap-2 mt-4">
                 <input type="checkbox" className="checkbox" />
                 <span className="label-text font-medium">
-                I acknowledge and accept the terms and conditions stated above.
+                  I acknowledge and accept the terms and conditions stated
+                  above.
                 </span>
               </label>
               <button className="btn btn-error w-[80px] text-white mt-4">
