58e9d457ab09dc71c9f7fb9cb28613fc03591c91
Prashanth <73587553+Prashanth-Santhanaraman@users.noreply.github.com>
2025-01-04 19:45:12 +0530

    Changed Mongodb URI
    Changed Mongodb URI

--- Backend/Server.js
+++ Backend/Server.js
@@ -18,8 +18,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/notes", auth, notesRoutes);
 app.use("/api/account", auth, accountRoutes);
 
-mongoose.connect("mongodb://127.0.0.1:27017/notes").then(() => {
+mongoose.connect(`mongodb+srv://221001113:${process.env.PASSWORD}@cluster0.f76qb.mongodb.net/`).then(() => {
   app.listen(port, (req, res) => {
     console.log("Connected to the MongoDB and Listening on port", port);
   });
 });
+
+//mongodb://127.0.0.1:27017/notes
