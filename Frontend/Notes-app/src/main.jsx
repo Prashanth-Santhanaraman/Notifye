76b5fa75fe078f20ee830342744bee4ab40d40e8
Prashanth <73587553+Prashanth-Santhanaraman@users.noreply.github.com>
2025-01-05 20:52:42 +0530

    Update main.jsx

--- Frontend/Notes-app/src/main.jsx
+++ Frontend/Notes-app/src/main.jsx
@@ -8,10 +8,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-    </BrowserRouter>
   </StrictMode>
 );
