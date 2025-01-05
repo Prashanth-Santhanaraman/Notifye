5e7c65e522c0cc081e0a5e3fa201f6482e4871f1
Prashanth <73587553+Prashanth-Santhanaraman@users.noreply.github.com>
2025-01-05 21:12:08 +0530

    Update main.jsx

--- Frontend/Notes-app/src/main.jsx
+++ Frontend/Notes-app/src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import App from "./App.jsx";
-import { BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>
+  </BrowserRouter>
 );
